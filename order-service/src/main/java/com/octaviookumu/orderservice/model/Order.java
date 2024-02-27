19d289da89900a4c014933c1531249b2b08c0995
octavian <octaviookumu@gmail.com>
2024-02-27 18:14:31 +0300

    Add order-service files

---
+++ order-service/src/main/java/com/octaviookumu/orderservice/model/Order.java
@@ -0,0 +1,17 @@
+package com.octaviookumu.orderservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+}
